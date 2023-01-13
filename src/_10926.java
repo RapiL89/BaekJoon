653c417cdfa5cd62cfdfcb1a9fe8c3db906b70ff
RapiL <sjw3090gmail.com>
2023-01-13 23:19:02 +0900

    2문제 추가!

---
+++ src/_10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _10926 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String in = scanner.next();
+
+        System.out.println(in + "??!");
+
+    }
+}
---
+++ src/_18108.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bul = sc.nextInt() - 543;
+
+        System.out.println(bul);
+
+
+
+    }
+}
